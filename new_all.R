44b56ab213f0d287a6be0861554ad7a041a16cf8
guilherme-marcelo <58813280+guilherme-marcelo@users.noreply.github.com>
2019-12-19 12:40:57 +0000

    Add files via upload

---
+++ new_all.R
@@ -0,0 +1,11 @@
+library(dplyr)
+library(lubridate)
+events <- readRDS("events_past_year.rds")
+events[,c("year","year_month")] <- c(format(events$local_date,"%Y"), format(events$local_date,"%Y-%m"))
+groupedevents <- group_by(events,year, year_month) 
+dffinal <- summarise(groupedevents,count_events=n())
+per_year <- aggregate(dffinal$count_events,by=list(year=dffinal$year),FUN=sum)
+pie(dffinal$count_events,dffinal$year_month)
+pie(per_year$x,per_year$year)
+
+  
