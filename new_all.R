393a905ff61770f140349fe5f569cdbca6061a42
guilherme-marcelo <58813280+guilherme-marcelo@users.noreply.github.com>
2019-12-19 12:56:42 +0000

    Update new_all.R

--- new_all.R
+++ new_all.R
@@ -5,7 +5,7 @@ events[,c("year","year_month")] <- c(format(events$local_date,"%Y"), format(even
 groupedevents <- group_by(events,year, year_month) 
 dffinal <- summarise(groupedevents,count_events=n())
 per_year <- aggregate(dffinal$count_events,by=list(year=dffinal$year),FUN=sum)
-pie(dffinal$count_events,dffinal$year_month)
-pie(per_year$x,per_year$year)
+pie(dffinal$count_events,dffinal$year_month,main="Count of events per month")
+pie(per_year$x,per_year$year,main="Count of events per year")
 
   
